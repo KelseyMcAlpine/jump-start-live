95c9447f7a49041da9fe7bab5bd75517630827db
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-01-31 10:47:03 -0800

    new ruby file

---
+++ exercises/optional-assessment.rb
@@ -0,0 +1,23 @@
+# Specifications:
+
+# Write a Ruby program that allows two users to play a game of
+# Nim together. If you are not familiar with the game start by
+# reading the Nim Wikipedia article.
+
+# At the start of the program you should accept each user's name.
+# And allow them to choose the game difficulty (easy = 2 piles,
+# medium = 3 piles, hard = 7 piles). Your game should then randomly
+# decide who will go first.
+
+# After the game finishes you should print each player's score
+# for that game (they earn +1 point for being the player that does
+# not remove the last counter) along with a summary of their moves.
+
+# Your game should then prompt them to play another game or quit.
+# If they choose to quit you should display total scores for each
+# player across all played games, otherwise you should allow the
+# two player's to play again.
+
+# Each user must select at least one item to remove from each pile
+# (they cannot remove 0 items). Make sure that users do not cheat,
+# and that their moves are valid.
