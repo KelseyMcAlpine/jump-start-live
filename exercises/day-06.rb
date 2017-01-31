0122324de728e7f083fde01ec019a738f53134c0
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-01-31 10:46:42 -0800

    new ruby file

---
+++ exercises/day-06.rb
@@ -0,0 +1,9 @@
+# Day 6 Exercises
+
+# Ask the user to enter the names, ages, and favorite colors of their
+# closest friends (you may not assume that the user's close friends
+# all have unique names). Output the total number of close friends
+# under 18, followed by their names. Output the number of unique
+# favorite colors, and then list them (Hint: Check out the uniq
+# method of the Array class in Ruby). Your solution should use at
+# least 1 hash and at least 1 array.
